362048a7b11ab9564ca623542f6e66238cab0cf6
Ville <villepikkarainen@Villes-MacBook-Air.local>
2018-11-22 14:45:55 +0200

    Chapter 4 and index updated
    Pushing chapter 4 and data wrangling of week 4

---
+++ data/create_human.R
@@ -0,0 +1,54 @@
+#Ville Pikkarainen 22.11.2018
+#IODS-project, data wrangling exercise as part of exercise 4, preparing dataset for chapter 5
+
+rm(list=ls()) 
+library(dplyr)
+#Importing data from UNDPs human development repors
+
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+class(hd)
+class(gii)
+#Structure and dimensions of the data
+str(hd)
+dim(hd)
+head(hd)
+summary(hd) #summary of variables in hd
+
+str(gii)
+dim(gii)
+head(gii)
+summary(gii) #summary of variables in gii
+#hd has 195 obs. of  8 variables
+#gii has 195 obs. of  10 variables
+
+#renaming the variables with shorter & descriptive names
+
+names(hd)
+names(gii)
+
+colnames(hd) <- c("hdirank","country","hdi","lifeexp","eduexp","edumean","gni","gniminusrank")
+colnames(gii) <- c("giirank", "country", "gii","matmor","adobirth","parlrep","edu2F","edu2M","labF","labM","edu2FM")
+
+#Create two new variables: ratio of female and male populations with secondary education in each country. (i.e. edu2F / edu2M). The second new 
+#variable should be the ratio of labour force participation of females and males in each country (i.e. labF / labM).
+
+gii <- mutate(gii, edu2FM = edu2F/edu2M)
+gii <- mutate(gii, labFM = labF/labM)
+
+#Joining together the two datasets using the variable "country" as the identifier. 
+#Keep only the countries  in both data sets (Hint: inner join). 
+#The joined data should have 195 observations and 19 variables. 
+#Call the new joined data "human" and save it in your data folder.
+head(gii)
+
+human <- inner_join(hd, gii, by = "country")
+str(human) 
+#dataset "human" has 195 obs. of  19 variables
+
+write.table(human, file = "human.txt", , dec=".", sep=",")
+human <- read.table("human.txt", sep = "," , header=TRUE)
+#calling "human"
+human
+
+#Everything seems to work!
